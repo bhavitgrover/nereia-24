05dc3f846015651141a8a5620a27e2ead65b8ee0
CasaN0va-bhavit <bhavit.grover@outlook.com>
2024-11-04 21:29:55 +0530

    made graph using plotly

--- routers/neritRouter.js
+++ routers/neritRouter.js
@@ -5,7 +5,9 @@ const router = require('express').Router(),
     {createBlock} = require('../utils/blockchain/blockchain')
 
 router.get('/', async (req, res) => {
-    console.log(createBlock('bhavit', 'arti chopra', 100, '627890c9042459af1db065dfb465e69113c53a0e2be2c5dd12b52172e2abbf7f'))
+    // console.log(createBlock('bhavit', 'arti chopra', 100, '627890c9042459af1db065dfb465e69113c53a0e2be2c5dd12b52172e2abbf7f'))
+    const reqWallet = await Wallet.findOne({mongooseId: req.user.id})
+    res.render('nerit', {user: req.user, wallet: reqWallet})
 })
 
 router.get('/createWallet', (req, res) => {
