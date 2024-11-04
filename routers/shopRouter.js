4d3fdff401f861d8ceb28ab027c697626e4c6faa
Ishit Rastogi <ishitrastogi@gmail.com>
2024-11-05 00:05:14 +0530

    basic backend for auction system

--- routers/shopRouter.js
+++ routers/shopRouter.js
@@ -1,7 +1,40 @@
 const router = require('express').Router()
+const Items = require('../schemas/auctionSchema.js')
+const { ensureRole } = require('../utils/authenticate.js')
+
 
 router.get('/', async (req,res) => {
-    res.render('shop', {title:'Shop', user:req.user})
+    let itemList = await Items.find()
+    console.log(itemList)
+    res.render('shop', {title: 'Marketplace', user: req.user, itemList})
+})
+
+router.get('/add', ensureRole, (req,res) => {
+    res.render('addItem', {title: 'Auction Items', user: req.user})
+})
+
+router.post('/add', async (req, res) => {
+    let {name, price, image, description} = req.body
+    let item = new Items({
+        createdBy: req.user.email,
+        product: name,
+        description: description,
+        price: price,
+        image: image,
+        biddingPrice: price,
+        highestBidder: req.user.fname,
+        live:false
+    })
+    await item.save()
+    res.redirect('/shop')
+})
+
+router.get('/live/:id', async (req,res) => {
+    let item = await Items.findOne({_id:req.params.id})
+    // console.log(item)
+    item.live = true
+    await item.save()
+    res.redirect('/shop')
 })
 
 module.exports = router
