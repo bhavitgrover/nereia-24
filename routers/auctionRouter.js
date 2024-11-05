7167e7b4a6a6e9169a6125bb51341226c8c49007
Ishit Rastogi <ishitrastogi@gmail.com>
2024-11-05 20:06:58 +0530

    ensure person has money for auction

--- routers/auctionRouter.js
+++ routers/auctionRouter.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const Items = require('../schemas/auctionSchema.js')
+const Users = require('../schemas/userSchema.js')
+const Wallets = require('../schemas/walletSchema.js')
 const { ensureRole } = require('../utils/authenticate.js')
 
 router.get("/:id", async (req, res) => {
   const item = await Items.findOne({_id:req.params.id})
   const currDate = Date.now()
+  const wallet = await Wallets.findOne({mongooseId:req.user._id})
   console.log(currDate)
   if(currDate - item.createdAt <= 20000 && item.live && !item.sold){
     console.log(item)
@@ -14,7 +17,8 @@ router.get("/:id", async (req, res) => {
     item.live = false
     item.sold = true
     await item.save()
-    res.render("auction", { title: "Auction", user: req.user, message:"This auction has ended!" });
+    const user = await Users.findOne({email:item.highestBidder})
+    res.render("auction", { title: "Auction", user: req.user, message:`This auction has ended!${item.product} was sold to ${user.fname}` });
   }
   else if(!item.live){
     res.render("auction", { title: "Auction", user: req.user, message:"This auction has not started yet!" });
@@ -23,12 +27,17 @@ router.get("/:id", async (req, res) => {
 
 router.post("/:id/buy", async (req, res) => {
   const item = await Items.findOne({_id:req.params.id})
-  console.log("BODUYDYDYDYD", req.body)
-  item.highestBidder = req.user.email
-  item.biddingPrice += Number(req.body.bid)
-  item.createdAt = Date.now()
-  await item.save()
-  res.redirect(`/auction/${req.params.id}`)
+  const wallet = await Wallets.findOne({mongooseId:req.user._id})
+  if(wallet.money < item.biddingPrice){
+    res.send(`gareeb sale paise ni h tujhpe 😠😠 bas ${wallet.money} nerit h tujhpe :joycat:`)
+  }
+  else{
+    item.highestBidder = req.user.email
+    item.biddingPrice += Number(req.body.bid)
+    item.createdAt = Date.now()
+    await item.save()
+    res.redirect(`/auction/${req.params.id}`)
+  }
 })
-
+//JAB KHARIDEGA TO PASIE KAM KAR CHUTIYE
 module.exports = router;
