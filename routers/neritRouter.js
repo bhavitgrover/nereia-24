1737a575641fbc7c0449e5f5d7dd38293f6eb1c3
CasaN0va-bhavit <83268213+CasaN0va-bhavit@users.noreply.github.com>
2024-11-06 12:20:43 +0530

    added pay functionality

--- routers/neritRouter.js
+++ routers/neritRouter.js
@@ -31,7 +31,13 @@ router.get('/', async (req, res) => {
     res.render('nerit', {user: req.user, wallet: reqWallet, nerit: reqNerit.neritValue, nerits: JSON.stringify(nerits), transactions: transactions})
 })
 
-router.get('/pay', async (req,res) => {
+router.get('/pay', async (req, res) => {
+    const wallet = await Wallet.findOne({mongooseId: req.user.id})
+    res.render('transaction', {user: req.user, wallet: wallet, error: ""})
+})
+
+router.post('/pay', async (req,res) => {
+    const {payeePublicKey, amount} = req.body
     const foundWallet = await Wallet.findOne({mongooseId: req.user.id})
     const allBlocks = await Block.find().sort({timeStamp: 'desc'})
     const lastBlock = allBlocks[0]
@@ -39,13 +45,13 @@ router.get('/pay', async (req,res) => {
     if (lastBlock) {
         lastHash = lastBlock.hash
     }
-    createBlock('MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAudltxCRk7Qdgor+3kpLRZUO2W+QOa6Cw7X5yn6yAlyRrjXFUjQSVtu1AMjozvwOt+eAy4dCizY91Khzn4KvI+AEwrV3QSX32DhHh92usXGAV4XAZF1a4v+totgGdeSt8CI/G7oyrEptuBtUWLY7cd43iXdXPntqBftqyPHaA355BHJuN7XJc1qc0YmTwAiVGKYrMS86mzCjeXLj+pDDM72QX/0q8FbOTZyhFVQWsalz4neAx4Ca2w8XJy62+sxfL4xtyvs7bdcoOgvmezaW1yQ0aXKmhVXi4kQfvKzah1ZjQndUpBPbEoHloqWV79AeJT9Z9E+oD6GmD1+A343JXEwIDAQAB', 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmxKd5/oakDDG8MJPoQ2kNqcI7itzW8jk578R9MpkxidTEriRYiQ55Ys9+th5YKXNWzqXpcvuquUdTR7+WjF+ayEiko48S5y/qBD1n/RFcoJAMWjWHxLoASDyc8yHuBc7b32gLy1SepFC2mn5WVo4HKHZTDR39CIEr+w48ZZ1HTrbHRnoHkPgI165SNPifcJ5w0a2A0rgboycHuuE4YnbkWsVNcNbKSmpmVAbCch17shFLbBesIbcrY8pwqW/V+1vZpAaNUIxXxYn+Ng2HC+fT1eXS9pDEzMRy7SiGeuxVCaaXOctqjwA9vJwVeVI4iLMp8ElqGErWurxgbRfu+30/QIDAQAB', 100, lastHash, foundWallet.privateKey, foundWallet.publicKey)
+    createBlock(foundWallet.publicKey ,payeePublicKey, amount, lastHash, foundWallet.privateKey, foundWallet.publicKey)
     .then(resp => {
         const data = JSON.parse(resp)
         if (data.success) {
-            res.send('transaction successful')
+            res.render('transaction',{error: 'Transaction Successful', user: req.user, wallet: foundWallet})
         } else {
-            res.send(data.message)
+            res.render('transaction',{error: data.message, user: req.user, wallet: foundWallet})
         }
     })
 })
